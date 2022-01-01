baef9f610edd3722fd0e9c0f2c67bac243552855
Andrey Salnikov <kvanta13@yandex.ru>
2022-01-01 23:28:11 +0300

    123

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -10,9 +10,15 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            Build build = new Build ("Москва", 2000, 2000, 6000);
+            Console.WriteLine("Введите адрес1");
+            string adress1 = Console.ReadLine();
+            Console.WriteLine("Введите адрес2");
+            string adress2 = Console.ReadLine();
+            Console.WriteLine("Введите этажность");
+            int floor = Convert.ToInt32(Console.ReadLine());
+            Build build = new Build (adress1, 2000, 2000, 6000);
             Console.WriteLine(build.Print());
-            MultiBuild MBuild = new MultiBuild ("Москва", 2000, 2000, 6000, 2);
+            MultiBuild MBuild = new MultiBuild (adress2, 2000, 2000, 6000, floor);
             Console.WriteLine(MBuild.Print());
             Console.ReadKey();
         }
